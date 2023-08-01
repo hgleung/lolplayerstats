57a278c9ebceaa4427eebe3cd51d93d1a5b2b0fd
hgleung <97071533+hgleung@users.noreply.github.com>
2023-07-31 23:40:09 -0700

    Add files via upload

---
+++ playerscraper.py
@@ -0,0 +1,79 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from reference import *
+from collections import defaultdict
+import csv
+
+pattern = r"title=\"(.*?)\""
+
+def get_match_history(player: str) -> list:
+    url = "https://lol.fandom.com/wiki/" + player + "/Match_History?so=search"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    table = soup.find("table", {"class": "wikitable"})
+    rows = table.find_all("tr")
+
+    result = []
+    for row in rows[4:54]:
+        cells = row.find_all("td")
+        if not str(cells[2].text).startswith("13"):
+            break
+        data = []
+        
+        for i in range(5):
+            data.append(cells[i].text)
+        
+        data.append((re.search(pattern, str(cells[6]))).group(1))
+        
+        data.append(cells[7].text)
+
+        data.append((re.search(pattern, str(cells[8]))).group(1))
+
+        for i in range(10, 13):
+            data.append(cells[i].text)
+
+        result.append(data)
+    
+    return result
+        
+def get_player_roster(team: str) -> list:
+    url = "https://lol.fandom.com/wiki/" + team
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    table = soup.find("table", {"class": "wikitable"})
+    rows = table.find_all("tr")
+
+    player_roster = []
+    for row in rows[1:]:
+        cells = row.find_all("td")
+        name = re.search(pattern, str(cells[2])).group(1).replace(" ", "_")
+        position = cells[4].text
+        player_roster.append((name, position))
+
+    return player_roster
+
+def convert_to_csv(player_name, region):
+    """Converts a list of lists into a .csv file.
+
+    Args:
+        player_roster: A list of lists containing the player data.
+        file_name: The name of the .csv file.
+    """
+
+    with open(f"players/{region}/{player_name}.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, delimiter=",")
+        writer.writerow(["Date", "Tournament", "Patch", "Result", "Side", "Opponent", "Game Duration", "Champ", "Kills", "Deaths", "Assists"])
+        match_history = get_match_history(player_name)
+        for game in match_history:
+            writer.writerow(game)
+
+if __name__ == "__main__":
+    for team in pcs_players:
+        for player in pcs_players[team]:
+            try:
+                convert_to_csv(player, "PCS")
+            except AttributeError:
+                pass
---
+++ reference.py
@@ -0,0 +1,25 @@
+pcs_teams = {
+    "Beyond_Gaming",
+    "CTBC_Flying_Oyster",
+    "Deep_Cross_Gaming",
+    "Dewish_Team",
+    "Frank_Esports",
+    "HELL_PIGS",
+    "Impunity",
+    "J_Team",
+    "PSG_Talon",
+    "West_Point_Esports_Philippines",
+}
+
+pcs_players = {
+    'PSG_Talon': ['Azhi', 'JunJia', 'Maple_(Huang_Yi-Tang)', 'Wako', 'Woody'], 
+    'Beyond_Gaming': ['LIKAI', 'Kongyue', '1116', 'Betty', 'Keres'], 
+    'J_Team': ['Driver', 'Lauva', 'Minji', 'Bruce', 'Enso'], 
+    'Dewish_Team': ['JP6RU8', 'WSL', 'Xiaotu', 'Pepero', 'EnHoa'], 
+    'West_Point_Esports_Philippines': ['Relhia', '665', 'NuL1', 'Prage', 'Cha'], 
+    'Deep_Cross_Gaming': ['Hanabi_(Su_Chia-Hsiang)', 'Hana', 'Mission', 'Cryscata', 'Orca_(Kuo_Cheng-Han)'], 
+    'CTBC_Flying_Oyster': ['Rest', 'Gemini', 'JimieN', 'Shunn', 'ShiauC'], 
+    'Impunity': ['LJW', 'Arashi', 'Shera', 'Blaze_(Jia_Xiang)', 'Felia', 'Smerv'], 
+    'Frank_Esports': ['Solokill', 'KennyChan', 'Pretender', 'MnM_(Wong_Ka_Chun)', 'Kaiwing'], 
+    'HELL_PIGS': ['Taco_(Fan_Zhao-Fu)', 'Godtone', 'Adiogs', 'Tide_(Hsieh_Yu-Che)', 'IC8']
+}
